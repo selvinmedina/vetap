8d1d15cb30f72eca568ac345d7941ab878b4c937
selvinmedina <selvinmedina0016@gmail.com>
2020-05-21 20:57:12 -0600

    work

--- models/models.py
+++ models/models.py
@@ -6,17 +6,35 @@ class vetapp_animal(models.Model):
     _name = 'vetapp.animal'
 
     name = fields.Char("Animal Name")
-    species = fields.Char("Species")
+    species_id = fields.Many2one("vetapp.species")
+    breed_id = fields.Many2one("vetapp.breed")
+    owner_id = fields.Many2one('res.partner')
     spoyorneuter = fields.Boolean("Spayed or Neutered")
     birthdate = fields.Date("Birth Date")
+    diagnosis_ids = fields.Many2many('vetapp.diagnosis')
+    notes = fields.Text()
+
+class vetapp_species(models.Model):
+    _name = "vetapp.species"
+    name = fields.Char("Species")
+    animal_ids = fields.One2many("vetapp.animal", "species_id")
+    notes = fields.Text()
+
+class vetapp_breed(models.Model):
+    _name = "vetapp.breed"
+    name = fields.Text()
+    animal_ids = fields.One2many("vetapp.animal", "breed_id")
+
+class vetapp_partner(models.Model):
+    _inherit = 'res.partner'
+    orientation_completed = fields.Boolean("Orientation Completed")
+    orientation_staff_id = fields.Many2one("res.users")
+    animal_ids = fields.One2many('vetapp.animal', 'owner_id', 'Pets')
+
+class vetapp_diagnosis(models.Model):
+    _name = 'vetapp.diagnosis'
+    name = fields.Char()
+    code = fields.Char('Diagnostic Code')
     notes = fields.Text()
 
 
-
-    # las propiedades computadas son con una funcion
-    # breed = fields.Float(compute="_value_pc", store=True)
-
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
